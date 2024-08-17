ab297f52fb0d16d5a41095cf73e72ab571ac8ae5
cecile hirschauer <cecile.h.75@gmail.com>
2024-08-17 12:09:46 +0200

    #Setting up the game => state : Crate struc Game and enums GameState and Sign

--- programs/tic-tac-toe/src/lib.rs
+++ programs/tic-tac-toe/src/lib.rs
@@ -1,5 +1,8 @@
 use anchor_lang::prelude::*;
 
+use num_derive::*;
+use num_traits::*;
+
 declare_id!("2VoQBkLfKDU3S7iqfo1Ub7NqBinC3fFBL8CDLqcvXz2J");
 
 #[program]
@@ -14,3 +17,26 @@ pub mod tic_tac_toe {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[account]
+pub struct Game {
+    players: [Pubkey; 2],           // (32 * 2)
+    turn: u8,                       // 1
+    board: [[Option<Sign>; 3]; 33], // 9 * (1 + 1) = 18
+    state: GameState,               // 32 + 1
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
+pub enum GameState {
+    Active,
+    Tie,
+    Won { winner: Pubkey },
+}
+
+#[derive(
+    AnchorSerialize, AnchorDeserialize, FromPrimitive, ToPrimitive, Copy, Clone, PartialEq, Eq,
+)]
+pub enum Sign {
+    X,
+    O,
+}
